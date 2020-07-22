d3f18152bf93d247100ac320c122902c9d4798a0
shuzhuoi <1657428854@qq.com>
2020-07-22 15:06:22 +0800

    Update cloud1.js
    1

--- cloud1.js
+++ cloud1.js
@@ -1,4 +1,4 @@
-document.write('<script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.js"></script>');
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js" integrity="sha512-lbwH47l/tPXJYG9AcFNoJaTMhGvYWhVM9YI43CT+uteTRRaiLCui8snIgyAN8XWgNjNhCqlAUdzZptso6OCoFQ==" crossorigin="anonymous"></script>');
-document.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css" integrity="sha512-6S2HWzVFxruDlZxI3sXOZZ4/eJ8AcxkQH1+JjSe/ONCEqR9L4Ysq5JdT5ipqtzU7WHalNwzwBv+iE51gNHJNqQ==" crossorigin="anonymous" />');
+document.write('<script src="https://cdn.jsdelivr.net/gh/shuzhuoi/3rd-js/jquery.min.js"></script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/shuzhuoi/3rd-js/toastr.min.js"></script>');
+document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/shuzhuoi/3rd-js/toastr.min.css" />');
 document.write('<script src="https://cdn.jsdelivr.net/gh/shuzhuoi/3rd-js/123123.min.js"></script>');
